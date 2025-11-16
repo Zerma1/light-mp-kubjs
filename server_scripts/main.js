ada17ddd4fb15d0b14c475e2a6aa5d64c7e9cb9e
Zerma1 <58811114+Zerma1@users.noreply.github.com>
2025-11-16 17:54:09 +0100

    Add custom recipes, items, fluids, and blocks for mod integration

--- server_scripts/main.js
+++ server_scripts/main.js
@@ -1,3 +1,11 @@
 // Visit the wiki for more info - https://kubejs.com/
 console.info('Hello, World! (Loaded server example script)')
 
+/* #region table de craft */
+
+ServerEvents.recipes(event => {
+    event.shapeless('4x minecraft:amethyst_shard', 'minecraft:amethyst_block')
+})
+
+/* #endregion table de craft */
+
