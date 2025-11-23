9ee3fbbd0aa6cfeaba9dbf7094086ce921cba2c1
Zerma1 <58811114+Zerma1@users.noreply.github.com>
2025-11-23 11:31:56 +0100

    Fix item registration event callback and comment out unused item definitions dellet folder craft

--- startup_scripts/Items.js
+++ startup_scripts/Items.js
@@ -1,10 +1,8 @@
 console.info('Chargement des items - Items')
 
-
-
 /* #region item */
 
-StartupEvents.registry('item', envent =>{
+StartupEvents.registry('item', event =>{ // ← Correction ici
 
     /* #region blaze head */
 
@@ -13,30 +11,30 @@ StartupEvents.registry('item', envent =>{
         .tooltip('§7Une tête de Blaze enflammée')
         .tooltip('§cTrès chaude au toucher!')
         .maxStackSize(16)
-        .rarity('rare')  // Options: common, uncommon, rare, epic
-        .glow(true)  // Fait briller l'item (effet enchanté)
-        .texture('minecraft:entity/blaze')
+        .rarity('rare')                         // Options: common, uncommon, rare, epic
+        .glow(true)                             // Fait briller l'item (effet enchanté)
+//        .texture('minecraft:entity/blaze')
 
     /* #endregion blaze head */
 
     /* #region Dragon Blood Neutral */
 
-    event.create('esi:iceandfire_dragon_neutral_blood')
-        .displayName('Neutral Dragon Blood')
-        .tooltip('Sang de dragon neutralisé')
-        .tooltip('§7Peut être transformé en sang élémentaire')  // §7 = gris
-        .maxStackSize(16)
-        .rarity('uncommon')
+//    event.create('esi:iceandfire_dragon_neutral_blood')
+//        .displayName('Neutral Dragon Blood')
+//        .tooltip('Sang de dragon neutralisé')
+//        .tooltip('§7Peut être transformé en sang élémentaire')  // §7 = gris
+//        .maxStackSize(16)
+//        .rarity('uncommon')
 
     /* #endregion Dragon Blood Neutral */
 
-    })
+})
 
 /* #endregion item */
 
 /* #region fluid */
 
-    StartupEvents.registry('fluid', event => {
+StartupEvents.registry('fluid', event => {
 
 //        /* #region Ultimate Lava */
 //
@@ -52,12 +50,12 @@ StartupEvents.registry('item', envent =>{
 //
 //        /* #endregion Ultimate Lava */
 
-    })
+})
 /* #endregion fluid */
 
 /* #region block */
 
-    StartupEvents.registry('block', event => {
+StartupEvents.registry('block', event => {
 
 //    /* #region blaze head */
 //
@@ -84,5 +82,5 @@ StartupEvents.registry('item', envent =>{
 //
 //    /* #endregion blaze head */
 
-    })
+})
 /* #endregion block */
