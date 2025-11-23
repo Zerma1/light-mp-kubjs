2a32066c2ac2859a14d3a2e2ce9c8dbad72c05de
Zerma1 <58811114+Zerma1@users.noreply.github.com>
2025-11-23 11:20:18 +0100

    Add item and fluid registrations for Blaze Head and Neutral Dragon Blood

---
+++ startup_scripts/LootTable.js
@@ -0,0 +1,14 @@
+console.info('Chargement des Loot Tables - LootTable')
+
+///* #region loot table */
+//
+//EntityEvents.drops('minecraft:blaze', event => {
+//
+//    event.addDrop('esi:blaze_head')
+//         .randomChance(0.05)    // 5% de chance
+//         .killedByPlayer()      // Seulement si tué par un joueur
+//         .affectedByLooting()   // Augmente avec Looting
+//         .count([0,1])          // quantiter entre 0 et 1
+//})
+//
+///* #endregion loot table */
