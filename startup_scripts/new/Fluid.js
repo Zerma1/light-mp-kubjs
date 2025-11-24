2362357ec7d83ecb26e1b9800b7f7424231ad17c
Zerma1 <58811114+Zerma1@users.noreply.github.com>
2025-11-24 21:09:48 +0100

    Comment out unused properties and bucket item customization for Ultimate Lava do not generate any error

--- startup_scripts/new/Fluid.js
+++ startup_scripts/new/Fluid.js
@@ -45,18 +45,19 @@ StartupEvents.registry('fluid', event => {
         .stillTexture('esi:fluid/ultimate_lava_still')
         .flowingTexture('esi:fluid/ultimate_lava_flow')
         .noBucket() // Le seau sera créé manuellement plus bas
-//        .luminosity(15)
+//        .lightLevel(15)
 //        .density(1500)
 //        .temperature(5000)
 //        .viscosity(6000)
 //        .rarity('rare')
+
         // Personnalisation du seau (à faire APRÈS la création du fluide)
-        .bucketItem.create('esi:ultimate_lava_bucket')
-           .displayName('Ultimate Lava Bucket')
-           .tooltip('§6Un seau de lave ultime§r') // §6 = or
-           .tooltip('§cContient une lave d\'une chaleur extrême')  // §c = rouge
-           .maxStackSize(1)
-           .rarity('epic')
+//        ultimateLava.bucketItem
+//           .displayName('Ultimate Lava Bucket')
+//           .tooltip('§6Un seau de lave ultime§r') // §6 = or
+//           .tooltip('§cContient une lave d\'une chaleur extrême')  // §c = rouge
+//           .maxStackSize(1)
+//           .rarity('epic')
 
 
 
